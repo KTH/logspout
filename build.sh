e22266ebe18b8be3ab5166fb93e206286b908bd0
Fredrik Jönsson <fjo@kth.se>
2016-11-07 22:26:24 +0100

    Oops, should only specify base package.

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ export GOPATH=/go
 go get
 go build -ldflags "-X main.Version $1" -o /bin/logspout
 apk del go git mercurial build-base
-apk add ca-certificates-20160104-r4
+apk add ca-certificates
 rm -rf /go
 rm -rf /var/cache/apk/*
 
